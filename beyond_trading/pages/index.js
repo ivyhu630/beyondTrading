1c928ef88cb7ab30d00d0aee5325e160791f7894
ivyhu <ajianouc@gmail.com>
2022-05-03 10:40:03 -0700

    intialize set up

---
+++ beyond_trading/pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Beyond Trading</title>
+        <meta name="description" content="Beyond Trading Welcome page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to Beyond Trading!
+        </h1>
+
+        <p className={styles.description}>
+          Get started by log in
+        </p>
+
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
