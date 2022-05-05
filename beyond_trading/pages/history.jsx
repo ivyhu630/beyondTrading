b0918c8ad31ae8b317c2d3160a0bca16ea6b83b2
ivyhu <ajianouc@gmail.com>
2022-05-04 22:59:39 -0700

    added logo

--- beyond_trading/pages/history.jsx
+++ beyond_trading/pages/history.jsx
@@ -71,10 +71,10 @@ export default function History() {
     style: 'currency',
     currency: 'USD',
   });
-  console.log(totalMarketValue);
+  console.log('market v ', totalMarketValue);
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className=" mt-5 w-75 text-sm text-left text-gray-500 ">
+      <table className=" pl-10 mt-5 w-75 text-sm text-left text-gray-500 history">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" className="px-6 py-3">
